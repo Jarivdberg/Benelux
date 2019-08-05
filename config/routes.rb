5a0a43368d989245a79327682f45f91754e5ebfe
Jari van den Berg <jarivandenberg@voormedia.com>
2019-08-05 22:12:20 +0200

    Switch to Postgresql

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "home#show"
   match "*path", to: "home#show", via: :all
+
+  namespace :api do
+    # resources :assets, only: [:index, :show], constraints: { id: /[A-Za-z0-9\.-]+?/ }
+  end
+
 end
